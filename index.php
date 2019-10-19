563311a2565e2381131687c46dc1bb4f22ea8d9d
Samir <39209311+MightyPelipper@users.noreply.github.com>
2019-10-18 19:52:44 -0500

    The basic webpage
    This contains all the current pages

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+    <!-- Link the stylesheet-->
+
+    <link rel="stylesheet" href="stylesheet.css">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:700" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Arimo" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+</head>
+
+<title>
+    Home
+</title>
+
+<body>
+
+   <p>test</p>
+
+</body>
